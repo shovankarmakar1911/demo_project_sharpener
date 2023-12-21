b56f98c2d78f5e1fd8960bac9fe971ea8eacf38d
shovankarmakar1911 <shovankarmakar1911@gmail.com>
2023-12-21 20:35:39 +0530

    getElementsByTagName Method

--- getElementsByTagName Method/index.js
+++ getElementsByTagName Method/index.js
@@ -1,9 +1,8 @@
 // Change the color of the 5th li tag (Mango) to blue
-var mangoLi = document.getElementsByTagName("li")[4]; // Note: Array index is 0-based
-mangoLi.style.color = "blue";
+var ListItems = document.getElementsByTagName("li");
+ListItems[4].style.color = "blue";
 
 // Make all li tags italic
-var allLiTags = document.getElementsByTagName("li");
-for (var i = 0; i < allLiTags.length; i++) {
-  allLiTags[i].style.fontStyle = "italic";
+for (var i = 0; i < ListItems.length; i++) {
+  ListItems[i].style.fontStyle = "italic";
 }
